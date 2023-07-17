2fbeca058c9a23f95497bda7301a79a37d1de104
Poludnica <svasti.kaula@gmail.com>
2023-07-17 14:16:28 +0300

    Add homework

---
+++ DZ/DZ_Fin.java
@@ -0,0 +1,16 @@
+
+
+
+public class DZ_Fin {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat("Василий", 5, 'm', 7.2);
+        Cat cat2 = new Cat("Мурлыка", 3, 'f', 4.5);
+
+        cat1.brain();
+        System.out.println(cat2.sleep());
+        cat1.infoCat();
+        cat2.infoCat();
+    }
+}
+    
+
